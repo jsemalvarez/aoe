85f8acb460fc240d2ea278543d07f582daa1afa2
Jose Alvarez W <jsemalvarez@gmail.com>
2020-12-09 15:51:45 -0300

    primera version PWA

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const App = () => {
+  return (
+    <main>
+        <h2>EL LIBRO NEGRO DEL</h2>
+        <h1>AoE</h1>
+        <h3>Todo lo que no hay que saber</h3>
+    </main>  
+  )
+}
